99db6313d1ff062a85a98a960ef3ab188549c511
Ehsan450 <ehsanananno32@gmail.com>
2023-07-05 18:08:00 +0600

    order controller added

--- src/main/java/com/example/ecommerce/controller/OrderController.java
+++ src/main/java/com/example/ecommerce/controller/OrderController.java
@@ -1,9 +1,45 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.entity.Order;
+import com.example.ecommerce.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping
 public class OrderController {
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/")
+    public ModelAndView orders(Model model) {
+        model.addAttribute("orders", this.orderService.orders());
+        return new ModelAndView("orders/orders");
+
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView order(@PathVariable("id") int id, Model model) {
+        Optional<Order> orderOptional = this.orderService.order(id);
+        if (orderOptional.isPresent()) {
+            Order order = orderOptional.get();
+            model.addAttribute("order", order);
+        }
+        return new ModelAndView("orders/order");
+
+    }
+
+
 }
